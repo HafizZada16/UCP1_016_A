c445c3b31b2a0f1bb66039d465809fa907047382
HafizZada16 <hafizzada16@gmail.com>
2025-05-13 18:19:58 +0700

    first commit

---
+++ contact.js
@@ -0,0 +1,7 @@
+function submitForm() {
+    alert("Pesan Anda terkirim");
+
+    // Mengosongkan form
+    const form = document.querySelector('form');
+    form.reset();
+}
---
+++ popup.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    setTimeout(function() {
+        alert("Promo Spesial! Dapatkan diskon 20% untuk semua sewa server Minecraft kami. Berlaku hingga akhir bulan!");
+    }, 1000); // Popup muncul setelah 1 detik
+};
